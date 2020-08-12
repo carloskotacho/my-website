6d36cad5cce608e280be6da0fda1a22f7b779cb4
carloskotacho <carloskotachoamd@hotmail.com>
2020-08-12 09:54:56 -0300

    refactor: organizing files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My website`,
+    description: `My first website`,
+    author: `@carloskotacho`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

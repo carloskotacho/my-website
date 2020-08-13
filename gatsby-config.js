9cfcb571e081c780e23d552a057267f40e9937e4
carloskotacho <carloskotachoamd@hotmail.com>
2020-08-13 14:06:34 -0300

    chore: add styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `@carlosk`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
